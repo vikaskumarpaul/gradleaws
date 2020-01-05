7612e926e2ced2874fabfd4f36971fc0538242f9
vikas paul <yogivikaspaul@gmail.com>
2020-01-05 12:40:35 +0530

    First Commit

---
+++ src/main/java/com/vikas/test/gradleaws/GradleawsApplication.java
@@ -0,0 +1,13 @@
+package com.vikas.test.gradleaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleawsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vikas/test/gradleaws/GradleawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikas.test.gradleaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
